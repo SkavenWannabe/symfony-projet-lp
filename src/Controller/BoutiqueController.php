3401fcb08a9377c83f119acd0f5fd9588563a0d0
SkavenWannabe <vincenzo.s.carminati@gmail.com>
2022-01-17 09:08:37 +0100

    tp3

---
+++ src/Controller/BoutiqueController.php
@@ -0,0 +1,33 @@
+<?php
+namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Service\BoutiqueService;
+
+class BoutiqueController extends AbstractController {
+
+    public function index(BoutiqueService $boutique) {
+        $categories = $boutique->findAllCategories();
+
+        return $this->render('categorie.html.twig',
+            ['categories' => $categories,]
+        );
+    }
+
+    public function rayon(BoutiqueService $boutique, int $idCategorie) {
+        $produits = $boutique->findProduitsByCategorie($idCategorie);
+
+        return $this->render('rayon.html.twig',
+            ['produits' => $produits,]
+        );
+    }
+
+    public function search(BoutiqueService $boutique, string $search) {
+        if(isset($_GET['search']))
+            $search = $_GET['search'];
+        $produits = $boutique->findProduitsByLibelleOrTexte($search);
+
+        return $this->render('rayon.html.twig',
+            ['produits' => $produits,]
+        );
+    }
+}
